8ac0e6912e7ab7b49d4892b2b3afafc070e67088
Giorgio Mariani <giorgiomariani94@gmail.com>
2025-10-08 15:21:42 +0200

    Update tests to include judge

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from datasets import load_dataset
 
+import pytest
+
 from glue3d.evaluate_answers import evaluate_GLUE3D_answers
 
 
@@ -27,7 +29,7 @@ def test_binary_task_evaluation():
     # Assert accuracy around 0.5 (random)
     gt["MODEL_ANSWER"] = False
     df = evaluate_GLUE3D_answers(task, gt)
-    assert np.abs(df.mean() - 0.5).item() <= 0.075
+    assert np.abs(df["VALUE"].mean() - 0.5).item() <= 0.075
 
 
 def test_multi_task_evaluation():
@@ -46,14 +48,13 @@ def test_multi_task_evaluation():
     # Assert accuracy around 0.5 (random)
     gt["MODEL_ANSWER"] = "A"
     df = evaluate_GLUE3D_answers(task, gt)
-    assert np.abs(df.mean() - 0.25).item() <= 0.05
+    assert np.abs(df["VALUE"].mean() - 0.25).item() <= 0.05
 
 
 def test_captioning_evaluation():
     task = "captioning_task"
 
     gt = _get_gt_data(task)
-    num_entries = len(gt)
 
     gt["MODEL_ANSWER"] = gt["ANSWER"]
     del gt["ANSWER"]
@@ -66,3 +67,14 @@ def test_captioning_evaluation():
 
     df = evaluate_GLUE3D_answers(task, gt)
     assert df["BLEU-1"].mean() <= 2.0
+
+
+@pytest.mark.test_judge
+def test_captioning_judge():
+    task = "captioning_task"
+    gt = _get_gt_data(task)
+    gt["MODEL_ANSWER"] = "The quick brown fox jumps over the lazy dog."
+    del gt["ANSWER"]
+
+    df = evaluate_GLUE3D_answers(task, gt, answer_evaluator="qwen_3_30B_A3B")
+    assert df["QWEN_SCORE"].mean() <= 2.0, df.columns
